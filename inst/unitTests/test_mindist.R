026e27a87e6f0e828cbb4bdfb158499e13c71001
wheelerb <33498685+wheelerb@users.noreply.github.com>
2024-02-28 16:37:30 -0500

    Add files via upload

--- inst/unitTests/test_mindist.R
+++ inst/unitTests/test_mindist.R
@@ -1,11 +1,7 @@
 test_mindist <- function() {
 
-    dir <- system.file("data", package="GrafGen", mustWork=TRUE)
-    f   <- file.path(dir, "train_results.rda")
-    load(f)
-
-    ref0 <- train_results[, "Refpop", drop=TRUE]
-    x    <- train_results
+    x    <- GrafGen:::getTrainResults()
+    ref0 <- x[, "Refpop", drop=TRUE]
     ref1 <- GrafGen:::rules.mindist(x)
 
     checkEquals(ref0, ref1)
--- inst/unitTests/test_neighbor.R
+++ inst/unitTests/test_neighbor.R
@@ -1,11 +1,8 @@
 test_neighbor <- function() {
 
-    dir <- system.file("data", package="GrafGen", mustWork=TRUE)
-    f   <- file.path(dir, "train_results.rda")
-    load(f)
-
-    tmp <- train_results[, "Refpop", drop=TRUE] %in% "hpgpAfrica"
-    x   <- train_results[tmp, , drop=FALSE]
+    dat <- GrafGen:::getTrainResults()
+    tmp <- dat[, "Refpop", drop=TRUE] %in% "hpgpAfrica"
+    x   <- dat[tmp, , drop=FALSE]
     x   <- GrafGen:::gp_nearest_neighbor(x)
 
     ref0 <- rep("hpgpAfroamerica", nrow(x))
