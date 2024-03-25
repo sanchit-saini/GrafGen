ca65e4ed315b041d91f531fc4de42f475156377d
wheelerb <33498685+wheelerb@users.noreply.github.com>
2024-03-25 10:24:12 -0400

    Delete inst directory

--- inst/unitTests/test_mindist.R
+++
@@ -1,9 +0,0 @@
-test_mindist <- function() {
-
-    x    <- GrafGen:::getTrainResults()
-    ref0 <- x[, "Refpop", drop=TRUE]
-    ref1 <- GrafGen:::rules.mindist(x)
-
-    checkEquals(ref0, ref1)
-
-}
